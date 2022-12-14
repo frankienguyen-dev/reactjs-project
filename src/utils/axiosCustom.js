f726ef757b116d3f16b4f6b15c1dda468df6f743
frankienguyen-dev <binguyen221198@gmail.com>
2022-12-14 20:16:01 +0700

    video 63

--- src/utils/axiosCustom.js
+++ src/utils/axiosCustom.js
@@ -1,7 +1,7 @@
-import axios from 'axios';
+import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: 'http://localhost:8081/',
+  baseURL: "http://localhost:8081/",
   //   headers: {
   //     'Content-Type': 'application/json',
   //   },
@@ -22,13 +22,11 @@ axiosClient.interceptors.request.use(
 // Add a response interceptor
 axiosClient.interceptors.response.use(
   function (response) {
-    console.log('response interceptor: ', response.data);
     // Any status code that lie within the range of 2xx cause this function to trigger
     // Do something with response data
     return response && response.data ? response.data : response;
   },
   function (error) {
-    console.log('error response', error.response);
     // Any status codes that falls outside the range of 2xx cause this function to trigger
     // Do something with response error
     return error && error.response && error.response.data

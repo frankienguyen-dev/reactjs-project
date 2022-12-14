f5e7c95584f7ca7dc390cfb2528b071d9541346c
frankienguyen-dev <binguyen221198@gmail.com>
2022-12-14 20:27:14 +0700

    update video 63 #2

--- src/components/Admin/Content/ModalUpdateUser.js
+++ src/components/Admin/Content/ModalUpdateUser.js
@@ -33,7 +33,9 @@ const ModalUpdateUser = (props) => {
       setImage("");
       setRole(dataUpdate.role);
       setUsername(dataUpdate.username);
-      setPreviewImage(`data:image/jpeg;base64,${dataUpdate.image}`);
+      if (dataUpdate.image) {
+        setPreviewImage(`data:image/jpeg;base64,${dataUpdate.image}`);
+      }
     }
   }, [dataUpdate]);
 

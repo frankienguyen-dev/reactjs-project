f9521377c39187f1752125c199fde60a24db22cf
frankienguyen-dev <binguyen221198@gmail.com>
2022-12-17 19:22:38 +0700

    video 70

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -4,8 +4,7 @@ import { FaBars } from 'react-icons/fa';
 
 import { useState } from 'react';
 import { Outlet } from 'react-router-dom';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+
 
 const Admin = (props) => {
   const [collapsed, setCollapsed] = useState(false);
@@ -28,20 +27,7 @@ const Admin = (props) => {
           <Outlet />
         </div>
       </div>
-      <ToastContainer
-        position="top-right"
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="light"
-      />
-      {/* Same as */}
-      <ToastContainer />
+      
     </div>
   );
 };

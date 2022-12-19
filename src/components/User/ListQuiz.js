2729ecb2d4db078f40f688cb9b8e0a851a6f9355
frankienguyen-dev <binguyen221198@gmail.com>
2022-12-19 19:49:35 +0700

    video 82

--- src/components/User/ListQuiz.js
+++ src/components/User/ListQuiz.js
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react";
 import { getQuizByUser } from "../../services/apiService";
 import "./ListQuiz.scss";
+import { useNavigate } from "react-router-dom";
 
 const ListQuiz = (props) => {
   const [arrQuiz, setArrQuiz] = useState([]);
 
+  const navigate = useNavigate();
   useEffect(() => {
     getQuizData();
   }, []);
@@ -35,7 +37,12 @@ const ListQuiz = (props) => {
               <div className="card-body">
                 <h5 className="card-title">Quiz {index + 1}</h5>
                 <p className="card-text">{quiz.description}</p>
-                <button className="btn btn-primary">Start now</button>
+                <button
+                  onClick={() => navigate(`/quiz/${quiz.id}`)}
+                  className="btn btn-primary"
+                >
+                  Start now
+                </button>
               </div>
             </div>
           );

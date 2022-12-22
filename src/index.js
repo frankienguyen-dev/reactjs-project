0e1f315b4b0e74398589338917d5cb5579f0cf5d
frankienguyen-dev <binguyen221198@gmail.com>
2022-12-23 06:59:42 +0700

    video 92

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import 'react-perfect-scrollbar/dist/css/styles.css';
+import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { store, persistor } from './redux/store';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Layout from './Layout';
+import 'nprogress/nprogress.css';
+import { PersistGate } from 'redux-persist/integration/react';
 
-import reportWebVitals from "./reportWebVitals";
-import { Provider } from "react-redux";
-import { store, persistor } from "./redux/store";
-import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Layout from "./Layout";
-import "nprogress/nprogress.css";
-import { PersistGate } from "redux-persist/integration/react";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>

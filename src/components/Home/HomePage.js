ceafe4cc6f7461bc129f74b3ac485c35050242f5
frankienguyen-dev <binguyen221198@gmail.com>
2023-01-04 05:10:46 +0700

    video 109 - update i18n homepage

--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -1,10 +1,13 @@
-import videoHomepage from "../../assets/video-homepage.mp4";
-import { useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
+import videoHomepage from '../../assets/video-homepage.mp4';
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { useTranslation, Trans } from 'react-i18next';
 
 const HomePage = (props) => {
   const isAuthenticated = useSelector((state) => state.user.isAuthenticated);
   const navigate = useNavigate();
+  const { t } = useTranslation();
+
   return (
     <div className="homepage-container">
       <div className="homepage-video">
@@ -14,18 +17,13 @@ const HomePage = (props) => {
       </div>
 
       <div className="homepage-content">
-        <div className="homepage-title">There's a better way to ask</div>
-        <div className="homepage-description">
-          You don't want to make a boring form. And your audience won't answer
-          one. Create a typeform instead—and make everyone happy.
-        </div>
+        <div className="homepage-title">{t('homepage.title1')}</div>
+        <div className="homepage-description">{t('homepage.title2')}</div>
         <div className="homepage-button">
           {isAuthenticated === true ? (
-            <button onClick={() => navigate("/users")}>Doing Quiz Now</button>
+            <button onClick={() => navigate('/users')}>{t('homepage.title3')}</button>
           ) : (
-            <button onClick={() => navigate("/login")}>
-              Get started - it's free
-            </button>
+            <button onClick={() => navigate('/login')}>{t('homepage.title4')}</button>
           )}
         </div>
       </div>
